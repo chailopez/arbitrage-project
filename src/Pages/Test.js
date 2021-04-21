7de47e3c4b327414d8e09b3fec39304d308e8b04
hplaserjetpro <pascal.egli97@gmail.com>
2021-04-21 16:28:26 +0200

    added Test.js

---
+++ src/Pages/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Test = () => {
+
+    return (
+        <div className="container">
+            <h1>Peter</h1>
+        </div>
+    )
+};
+
+export default Test;
